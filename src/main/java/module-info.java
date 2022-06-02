2ddbc178d988768d9f89ba81fe230b0d58bbc9df
Richard <43894138+Richard-Chimama@users.noreply.github.com>
2022-06-02 23:58:17 +0200

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ramdomstring {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ramdomstring to javafx.fxml;
-    exports com.example.ramdomstring;
-}
